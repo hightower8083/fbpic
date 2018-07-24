465e8e71e4947cde0bf2bb998d696f0572b19eb5
hightower8083 <igor.andriyash@gmail.com>
2018-07-24 17:05:02 +0300

    added template for gathering routines on GPU bug-fix

--- fbpic/particles/templater/make_gather_cuda.py
+++ fbpic/particles/templater/make_gather_cuda.py
@@ -4,8 +4,8 @@ from fbpic.particles.templater import __path__
 
 src_path = __path__[0]+'/'
 
-E_arg = "                           Er_m{0}, Et_m{0}, Ez_m{0},"
-B_arg = "                           Br_m{0}, Bt_m{0}, Bz_m{0},"
+E_arg = "                    Er_m{0}, Et_m{0}, Ez_m{0},"
+B_arg = "                    Br_m{0}, Bt_m{0}, Bz_m{0},"
 
 E_docstr = \
 """    Er_m{0}, Et_m{0}, Ez_m{0} : 2darrays of complexs
@@ -16,23 +16,23 @@ B_docstr = \
         The magnetic fields on the interpolation grid for the mode {0}"""
 
 
-mode_src = \
+mode_lin_src = \
 """
         # E-Field
         # -------
         Fr = 0.
         Ft = 0.
         Fz = 0.
-        # Add contribution from mode m
+        # Add contribution from mode {0}
         Fr, Ft, Fz = add_linear_gather_for_mode( {0},
             Fr, Ft, Fz, exptheta_m, Er_m{0}, Et_m{0}, Ez_m{0},
             iz_lower, iz_upper, ir_lower, ir_upper,
             S_ll, S_lu, S_lg, S_ul, S_uu, S_ug )
         # Convert to Cartesian coordinates
         # and write to particle field arrays
-        Ex += cos*Fr - sin*Ft
-        Ey += sin*Fr + cos*Ft
-        Ez += Fz
+        Ex[i] += cos*Fr - sin*Ft
+        Ey[i] += sin*Fr + cos*Ft
+        Ez[i] += Fz
 
         # B-Field
         # -------
@@ -41,25 +41,60 @@ mode_src = \
         Fr = 0.
         Ft = 0.
         Fz = 0.
-        # Add contribution from mode m
+        # Add contribution from mode {0}
         Fr, Ft, Fz = add_linear_gather_for_mode( {0},
             Fr, Ft, Fz, exptheta_m, Br_m{0}, Bt_m{0}, Bz_m{0},
             iz_lower, iz_upper, ir_lower, ir_upper,
             S_ll, S_lu, S_lg, S_ul, S_uu, S_ug )
         # Convert to Cartesian coordinates
         # and write to particle field arrays
-        Bx += cos*Fr - sin*Ft
-        By += sin*Fr + cos*Ft
-        Bz += Fz
+        Bx[i] += cos*Fr - sin*Ft
+        By[i] += sin*Fr + cos*Ft
+        Bz[i] += Fz
 
-        # Increase index of the azimuthal complex factor
-        exptheta_m *= exptheta_1
+        exptheta_m *= exptheta_m1
+"""
+
+mode_cub_src = \
+"""
+        # E-Field
+        # -------
+        Fr = 0.
+        Ft = 0.
+        Fz = 0.
+        # Add contribution from mode {0}
+        Fr, Ft, Fz = add_cubic_gather_for_mode( {0},
+            Fr, Ft, Fz, exptheta_m, Er_m{0}, Et_m{0}, Ez_m{0},
+            ir_lowest, iz_lowest, Sr, Sz, Nr, Nz )
+        # Convert to Cartesian coordinates
+        # and write to particle field arrays
+        Ex[i] += cos*Fr - sin*Ft
+        Ey[i] += sin*Fr + cos*Ft
+        Ez[i] += Fz
+
+        # B-Field
+        # -------
+        # Clear the placeholders for the
+        # gathered field for each coordinate
+        Fr = 0.
+        Ft = 0.
+        Fz = 0.
+        # Add contribution from mode {0}
+        Fr, Ft, Fz =  add_cubic_gather_for_mode( {0},
+            Fr, Ft, Fz, exptheta_m, Br_m{0}, Bt_m{0}, Bz_m{0},
+            ir_lowest, iz_lowest, Sr, Sz, Nr, Nz )
+        # Convert to Cartesian coordinates
+        # and write to particle field arrays
+        Bx[i] += cos*Fr - sin*Ft
+        By[i] += sin*Fr + cos*Ft
+        Bz[i] += Fz
+
+        exptheta_m *= exptheta_m1
 """
 
 
-def make_gather_push_cuda( Nm, module_file='./_gather_push_cuda_.py',
-                 template_file='template_gather_push_cuda.py'):
-
+def make_gather_cuda( Nm, module_file='./_gather_cuda_.py',
+                 template_file='template_gather_cuda.py'):
 
     kernel_template = Template(filename=src_path+template_file)
     mrange = range(0, Nm)
@@ -71,7 +106,8 @@ def make_gather_push_cuda( Nm, module_file='./_gather_push_cuda_.py',
     Args['E_docstr'] = '\n\n'.join([E_docstr.format(m) for m in mrange])
     Args['B_docstr'] = '\n\n'.join([B_docstr.format(m) for m in mrange])
 
-    Args['add_mode'] = ''.join([mode_src.format(m) for m in mrange])
+    Args['add_mode_lin'] = ''.join([mode_lin_src.format(m) for m in mrange])
+    Args['add_mode_cub'] = ''.join([mode_cub_src.format(m) for m in mrange])
 
     f = open(module_file, mode='w')
     f.writelines( kernel_template.render(**Args) )
